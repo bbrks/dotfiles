09923f5db6398f82e18eb030b862d69238bf5626
Ben Brooks <ben@bbrks.me>
2018-05-09 23:33:12 +0100

    Add minimap packages for atom

--- atom/init.sh
+++ atom/init.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-apm install go-plus go-tip go-debug file-icons gruvbox-plus-syntax pristine-ui
+apm install go-plus go-tip go-debug file-icons minimap minimap-git-diff minimap-linter linter gruvbox-plus-syntax pristine-ui
